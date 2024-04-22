88a89c8096c243f2cb95dde0f29493d463c6af56
知念英弥 <s19013@std.it-college.ac.jp>
2024-04-22 23:04:25 +0900

    エラーログを出すように編集

--- controller/healthz.go
+++ controller/healthz.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/model"
@@ -10,12 +11,13 @@ import (
 func Healthz(writer http.ResponseWriter, req *http.Request) {
 
 	// レスポンスを生成する
-	response := model.HealthzResponse{Message: "ok"}
+	response := model.HealthzResponse{Message: "OK"}
 
 	// データ構造をJSONにエンコードしてHTTPレスポンスとして返す
 	writer.Header().Set("Content-Type", "application/json")
 	err := json.NewEncoder(writer).Encode(response)
 	if err != nil {
+		log.Println(err)
 		return
 	}
 
