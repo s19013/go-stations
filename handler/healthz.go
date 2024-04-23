9c055fa1d13f4a85c7c39c608964d613545cb5a4
知念英弥 <s19013@std.it-college.ac.jp>
2024-04-23 10:28:51 +0900

    問題文をよく見るとここにレスポンス処理を書けと書いてあったので修正

--- handler/healthz.go
+++ handler/healthz.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/model"
@@ -16,5 +18,14 @@ func NewHealthzHandler() *HealthzHandler {
 
 // ServeHTTP implements http.Handler interface.
 func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_ = &model.HealthzResponse{}
+	// レスポンスを生成する
+	response := model.HealthzResponse{Message: "OK"}
+
+	// データ構造をJSONにエンコードしてHTTPレスポンスとして返す
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
