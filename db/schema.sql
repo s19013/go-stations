4a32c61a4cad1f7d50c779f7f25c0337903779b0
知念英弥 <s19013@std.it-college.ac.jp>
2024-04-23 11:40:20 +0900

    checkについて知らなかったのでメモを残す｡ フォーマッターのせいで改行が変

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,22 @@
 CREATE TABLE IF NOT EXISTS todos (
-  id          INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
-  subject     TEXT     NOT NULL,
-  description TEXT     NOT NULL DEFAULT '',
-  created_at  DATETIME NOT NULL DEFAULT (DATETIME('now')),
-  updated_at  DATETIME NOT NULL DEFAULT (DATETIME('now')),
-  CHECK(subject <> '')
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  subject TEXT NOT NULL,
+  description TEXT NOT NULL DEFAULT '',
+  created_at DATETIME NOT NULL DEFAULT (DATETIME('now')),
+  updated_at DATETIME NOT NULL DEFAULT (DATETIME('now')),
+  CHECK(subject <> '') -- check成約でデータを追加する時に値が指定した条件を満たしているかどうかのチェック
+  -- この場合は､subject列が空でないことを確認する
 );
 
-CREATE TRIGGER IF NOT EXISTS trigger_todos_updated_at AFTER UPDATE ON todos
-BEGIN
-  UPDATE todos SET updated_at = DATETIME('now') WHERE id == NEW.id;
+CREATE TRIGGER IF NOT EXISTS trigger_todos_updated_at
+AFTER
+UPDATE
+  ON todos BEGIN
+UPDATE
+  todos
+SET
+  updated_at = DATETIME('now')
+WHERE
+  id == NEW.id;
+
 END;
