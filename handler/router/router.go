e0af63f6fb26b0dd67a6b46e733e9bee30007b5e
知念英弥 <s19013@std.it-college.ac.jp>
2024-04-23 10:29:49 +0900

    handlerを編集したことにともなう修正

--- handler/router/router.go
+++ handler/router/router.go
@@ -4,14 +4,14 @@ import (
 	"database/sql"
 	"net/http"
 
-	"github.com/TechBowl-japan/go-stations/controller"
+	"github.com/TechBowl-japan/go-stations/handler"
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	// register routes
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/healthz", controller.Healthz)
+	mux.HandleFunc("/healthz", handler.NewHealthzHandler().ServeHTTP)
 
 	return mux
 }
