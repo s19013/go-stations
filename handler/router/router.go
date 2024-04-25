e5f536791cba0261429808ac114f1addd21d1ca6
知念英弥 <s19013@std.it-college.ac.jp>
2024-04-25 17:14:12 +0900

    todoの内容を返すまで実装した

--- handler/router/router.go
+++ handler/router/router.go
@@ -4,10 +4,12 @@ import (
 	"database/sql"
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/handler"
 	"github.com/TechBowl-japan/go-stations/model"
+	"github.com/TechBowl-japan/go-stations/service"
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
@@ -17,7 +19,10 @@ func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	mux.HandleFunc("/healthz", handler.NewHealthzHandler().ServeHTTP)
 
 	mux.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
+		todoService := service.NewTODOService(todoDB)
+		todoHandler := handler.NewTODOHandler(todoService)
+
+		if r.Method == http.MethodPost {
 			// body取り出し
 			body, err := io.ReadAll(r.Body)
 			if err != nil {
@@ -27,8 +32,10 @@ func NewRouter(todoDB *sql.DB) *http.ServeMux {
 
 			// json変化
 			var request model.CreateTODORequest
-			err2 := json.Unmarshal([]byte(body), &request)
+			err2 := json.Unmarshal(body, &request)
+			// err2 := json.NewDecoder(bytes.NewReader(body)).Decode(&request)
 			if err2 != nil {
+				log.Println("err2", err2)
 				http.Error(w, "Failed to parse JSON body", http.StatusBadRequest)
 				return
 			}
@@ -40,6 +47,18 @@ func NewRouter(todoDB *sql.DB) *http.ServeMux {
 			}
 
 			// 登録してtodoを受け取る
+			response, err3 := todoHandler.Create(r.Context(), &request)
+			if err3 != nil {
+				log.Println("err3", err3)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			err4 := json.NewEncoder(w).Encode(response)
+			if err4 != nil {
+				log.Println("err4", err4)
+				return
+			}
 
 		}
 	})
